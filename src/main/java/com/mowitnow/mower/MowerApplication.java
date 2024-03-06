8968330c30df37b8874cc2546d40aaec6e1bc76f
Malik <benanesmalek@gmail.com>
2024-03-06 04:26:13 +0100

    :hammer: initial commit

---
+++ src/main/java/com/mowitnow/mower/MowerApplication.java
@@ -0,0 +1,10 @@
+package com.mowitnow.mower;
+
+public class MowerApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from mower application!");
+    }
+
+}
